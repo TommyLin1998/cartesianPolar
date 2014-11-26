fe45706076b4385043a8038f384e5bc91ba3e52b
Tommy Lin <tommylin1998@outlook.com>
2014-11-26 16:35:28 -0500

    Basically finish the cartesian coordinate

--- cartesianPolar.py
+++ cartesianPolar.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-np.pi, np.pi, 100)
+
 plt.subplot(1, 2, 1)
 # Plot the rectangular coordinates here
+y_sin = np.sin(x)
+y_sin2 = np.sin(x) * np.sin(x)
+y_cos = np.cos(x)
+
+plt.plot(x, y_sin, 'r')
+plt.plot(x, y_sin2, 'g')
+plt.plot(x, y_cos, 'b')
+
+plt.show()
 
 plt.subplot(1, 2, 2, polor = True)
 # Plot the polar coordinates here
