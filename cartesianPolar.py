146955795fefcea49926adb07c575d395e4babce
Tommy Lin <tommylin1998@outlook.com>
2014-11-28 12:35:12 -0500

    Finish the polar coordinate

--- cartesianPolar.py
+++ cartesianPolar.py
@@ -17,7 +17,16 @@ plt.plot(x, y_cos, 'b', label = 'cosine')
 
 plt.legend(loc = 'lower left')
 
-plt.show()
-
-plt.subplot(1, 2, 2, polor = True)
+plt.subplot(1, 2, 2, polar = True)
 # Plot the polar coordinates here
+y_sin = np.sin(x)
+y_sin2 = np.sin(x) * np.sin(x)
+y_cos = np.cos(x)
+
+plt.plot(x, y_sin, 'r', label = 'sine')
+plt.plot(x, y_sin2, 'g', label = 'sine squared')
+plt.plot(x, y_cos, 'b', label = 'cosine')
+
+plt.legend(loc = 'lower left')
+
+plt.show()
